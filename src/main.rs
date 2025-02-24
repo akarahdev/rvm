e3ec8b16c81d79876ae8050ca3d3b0b26cbadf8d
akarahdev <endisticbutitsforgit@gmail.com>
2025-02-23 23:05:13 -0500

    feat: add basic ByteBuffer API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Error;
+
+use buf::{ByteBuffer, ReadBuf};
+use image::Image;
+
+pub mod buf;
+pub mod image;
+
+fn main() -> Result<(), Error> {
+    match std::fs::read("./image.rvm") {
+        Ok(image) => {
+            let mut image = ByteBuffer::new(&image);
+            let image = Image::read_rwbuf(&mut image)?;
+            println!("{:?}", image);
+            Ok(())
+        }
+        Err(_) => {
+            let image = Image {
+                hostname: "somepc".to_string(),
+            };
+            let mut buf = ByteBuffer::empty();
+            buf.write(&image)?;
+
+            std::fs::write("./image.rvm", buf)?;
+            Ok(())
+        }
+    }
 }
